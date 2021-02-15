17f57131dc9476ebe430104aac560e98716f00e6
ginyusquad <esquadraoginyuimpacta@gmail.com>
2021-02-15 21:44:02 +0000

    Alteração para lista não ordenada

--- src/arranjos/Arranjo.java
+++ src/arranjos/Arranjo.java
@@ -6,18 +6,6 @@ public class Arranjo {
 
 	public static final Integer[] LISTA_INICIAL = { 940, 880, 830, 790, 750, 660, 650, 590, 510, 940 };
 
-	// Lista Ordenada
-	public Integer[] listOrdenada;
-
-	// Construtor: Função que é chamada na hora de => new Arranjo();
-	public Arranjo() {
-
-		// Chama um função para ordenar a Lista inicial e guarda na variavel
-		// listOrdenada
-		listOrdenada = ordenarLista(LISTA_INICIAL);
-
-	}
-
 	/**
 	 * Função que retorna o menor valor do arranjo
 	 * 
@@ -25,8 +13,14 @@ public class Arranjo {
 	 */
 	public int menor() {
 
-		int menor = listOrdenada[0];
+        int menor = LISTA_INICIAL[0];
+        
+		for (Integer numero : LISTA_INICIAL) {
 
+			// Procura o menor
+			if (menor > numero)menor = numero;
+
+		}
 		return menor;
 	}
 
@@ -37,7 +31,14 @@ public class Arranjo {
 	 */
 	public int maior() {
 
-		int maior = listOrdenada[listOrdenada.length - 1];
+		int maior = LISTA_INICIAL[0];
+
+        for (Integer numero : LISTA_INICIAL) {
+
+			// Procura o menor
+			if (maior < numero)maior = numero;
+
+		}
 
 		return maior;
 	}
@@ -51,7 +52,7 @@ public class Arranjo {
 
 		int soma = 0;
 
-		for (Integer numero : listOrdenada) {
+		for (Integer numero : LISTA_INICIAL) {
 			soma = numero + soma;
 		}
 
@@ -68,7 +69,7 @@ public class Arranjo {
 
 		int repeticoes = 0;
 
-		for (Integer numero : listOrdenada) {
+		for (Integer numero : LISTA_INICIAL) {
 
 			// Incrementa o contador de repetições para cada achado
 			if (numeroRepetido == numero)
@@ -77,38 +78,5 @@ public class Arranjo {
 		}
 
 		return repeticoes;
-	}
-
-	public Integer[] ordenarLista(Integer[] lista) {
-		Integer[] listaOrdenada = lista.clone();
-
-		// Inicia a ordenação da LISTA_INICIAL
-		int auxiliar = 0;
-		for (int i = 0; i < listaOrdenada.length; i++) {
-
-			for (int j = 0; j < (listaOrdenada.length - 1); j++) {
-				// Verifica se o valor a frente e maior que o do index atual
-				if (listaOrdenada[j] > listaOrdenada[j + 1]) {
-					auxiliar = listaOrdenada[j];
-					// Troca os valores para ordenar
-					listaOrdenada[j] = listaOrdenada[j + 1];
-					listaOrdenada[j + 1] = auxiliar;
-
-				}
-			}
-
-		}
-
-		return listaOrdenada;
-	}
-
-	// Construtor para caso o usuario queira adicionar uma nova lista para o arranjo
-	// que seja diferente da LISTA_INICIAL
-	public Arranjo(Integer[] lista) {
-
-		// Chama um função para ordenar a Lista inicial e guarda na variavel
-		// listOrdenada
-		listOrdenada = ordenarLista(lista);
-
 	}
 }
